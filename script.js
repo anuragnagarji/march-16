1cdb337c64315876055f73de8d35f5f2c533ecc5
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2024-03-16 21:41:45 +0530

    Update script.js
    add script

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", profession:"developer" },
-  { id: 2, name: "jack", age: "20", profession:"developer" },
-  { id: 3, name: "karen", age: "19", profession:"admin" },
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
-// console.log(arr[0].marks);
-function PrintStudentswithMap() {
+
+function PrintDeveloperbyMap() {
   arr.map((item)=>{
-      if(item.profession=="developer"){
-      console.log(item.name+" "+item.marks);
-      }
-  });
+    if(item.profession=="developer"){
+    console.log(item.name);
+    }
+});
   //Write your code here , just console.log
 }
 
-function PrintStudentsbyForEach() {
+function PrintDeveloperbyForEach() {
   arr.forEach((item)=>{
-      if(item.profession=="developer"){
-      console.log(item.name);
-      }
-  });
+    if(item.profession=="developer"){
+    console.log(item.name);
+    }
+});
   //Write your code here , just console.log
 }
 
 function addData() {
   arr.push({id:4,name:"susan",age:"20",profession:"intern"});
-console.log(arr);  
-}
-
-function removeFailedStudent() {
-  arr=arr.filter((item)=>{
-      if((item.profession)!="admin"){
-          return item;
-      }
-  });
-  console.log(arr); 
+console.log(arr); 
   //Write your code here, just console.log
 }
+
+function removeAdmin() {
+  arr=arr.filter((item)=>{
+    if((item.profession)!="admin"){
+        return item;
+    }
+});
+console.log(arr); 
+  //Write your code here, just console.log
+}
+
 let arr1 = [
   { id: 7, name: "ravi", age: "18", profession:"director" },
   { id: 8, name: "raj", age: "20", profession:"clerk" },
@@ -48,3 +50,4 @@ arr= arr.concat(arr1);
  console.log(arr);
   //Write your code here, just console.log
 }
+
